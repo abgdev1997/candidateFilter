7761c600dac130207bb8fdc8b0b850525f6e6302
abgdev1997 <adrianblancogarcia97@gmail.com>
2022-01-26 14:40:56 +0100

    lets go!

---
+++ src/main/java/com/abgdev1997/candidateFilter/CandidateFilterApplication.java
@@ -0,0 +1,13 @@
+package com.abgdev1997.candidateFilter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandidateFilterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CandidateFilterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abgdev1997/candidateFilter/CandidateFilterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abgdev1997.candidateFilter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandidateFilterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
