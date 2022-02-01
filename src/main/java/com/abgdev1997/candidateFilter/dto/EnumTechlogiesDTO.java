44e000402e32d8b5d49728bd73cdaf3930b89851
abgdev1997 <adrianblancogarcia97@gmail.com>
2022-02-01 14:43:11 +0100

    applicants classes added

---
+++ src/main/java/com/abgdev1997/candidateFilter/dto/EnumTechlogiesDTO.java
@@ -0,0 +1,23 @@
+package com.abgdev1997.candidateFilter.dto;
+
+enum EnumTechlogiesDTO {
+    ANGULAR,
+    JAVA,
+    JAVASCRIPT,
+    REACT,
+    SPRING,
+    VUE,
+    PYTHON,
+    HTML,
+    CSS,
+    NODEJS,
+    DOCKER,
+    PHP,
+    GIT,
+    HIBERNATE,
+    BIGDATA,
+    SYMFONY,
+    TYPESCRIPT,
+    NET,
+    DJANGO
+}
---
+++ src/main/java/com/abgdev1997/candidateFilter/models/Applicants.java
@@ -0,0 +1,130 @@
+package com.abgdev1997.candidateFilter.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.ArrayList;
+
+@Entity
+public class Applicants {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String name;
+    String country;
+    String city;
+    String phone;
+    String email;
+    String presence;
+    Boolean relocation;
+    String image;
+    String pdf;
+    ArrayList<?> technologies = new ArrayList<>();
+
+    public Applicants() {
+    }
+
+    public Applicants(Long id, String name, String country, String city, String phone, String email, String presence, Boolean relocation, String image, String pdf, ArrayList<?> technologies) {
+        this.id = id;
+        this.name = name;
+        this.country = country;
+        this.city = city;
+        this.phone = phone;
+        this.email = email;
+        this.presence = presence;
+        this.relocation = relocation;
+        this.image = image;
+        this.pdf = pdf;
+        this.technologies = technologies;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPresence() {
+        return presence;
+    }
+
+    public void setPresence(String presence) {
+        this.presence = presence;
+    }
+
+    public Boolean getRelocation() {
+        return relocation;
+    }
+
+    public void setRelocation(Boolean relocation) {
+        this.relocation = relocation;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getPdf() {
+        return pdf;
+    }
+
+    public void setPdf(String pdf) {
+        this.pdf = pdf;
+    }
+
+    public ArrayList<?> getTechnologies() {
+        return technologies;
+    }
+
+    public void setTechnologies(ArrayList<?> technologies) {
+        this.technologies = technologies;
+    }
+}
