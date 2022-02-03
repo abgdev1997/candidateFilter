97c9322c4d536f72f0f7f9c1b42338723ba82d82
abgdev1997 <adrianblancogarcia97@gmail.com>
2022-02-03 18:09:47 +0100

    filter added and done pending testing

--- src/main/java/com/abgdev1997/candidateFilter/repository/ApplicantsRepository.java
+++ src/main/java/com/abgdev1997/candidateFilter/repository/ApplicantsRepository.java
@@ -4,7 +4,9 @@ import com.abgdev1997.candidateFilter.models.Applicants;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Repository
 public interface ApplicantsRepository extends JpaRepository<Applicants, Long> {
-
 }
